fca48b612742e0da68081cc119f9b921bf5a2071
Omar <164964816+8omz@users.noreply.github.com>
2024-07-10 21:02:03 +0000

    Added colision detection

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ const ctx = canvas.getContext("2d");
 const road =new Road(canvas.width/2,canvas.width*0.9)
 const car =new Car(road.getLaneCenter(1),100,30,50);
 
-animate();
 
+animate();
 function animate(){
-    car.update();
+    car.update(road.borders);
 
     canvas.height = window.innerHeight;
     
